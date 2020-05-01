33d7f3bfc0ffa551ebb1148c68bb43fc23daa1cb
onlylonely32 <borderinvais@gmail.com>
2020-05-01 18:08:28 +0300

    add tic tac toe game

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let tBtn = document.getElementById("tic-tac");
+let gBtn = document.getElementById("gallows");
+
+let main = document.getElementById("main");
+
+tBtn.addEventListener("mouseover", function() {
+ main.style.background = "url('./img//tic-tac.jpg') center center no-repeat #EEEEEE";
+ main.style.opacity = "1"
+});
+
+tBtn.addEventListener("mouseleave", function() {
+ main.style.opacity = "0"
+});
+
+gBtn.addEventListener("mouseover", function() {
+ main.style.background = "url('./img/gallows.jpg') center center no-repeat #EEEEEE";
+ main.style.opacity = "1"
+});
+
+gBtn.addEventListener("mouseleave", function() {
+ main.style.opacity = "0"
+});
